ae6d48fdc0a704311db55a09eb44c031a3861f4a
farhan sharif <farhansharif1410@gmail.com>
2022-11-06 20:17:09 +0530

    server is runnig now

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('MongoDB Server is Running')
+})
+
+app.listen(port, () => {
+    console.log('Server is Running on port ', port);
+})
